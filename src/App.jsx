214ae2d5ff64b2bdcbcce0e0de18d5d841ffafe3
Martin Kamír <martin.k.kamir@gmail.com>
2023-06-09 20:48:50 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import * as math from "mathjs";
 
 const buttons = [
-    {id: 'ce', value: 'CE'},
-    {id: "clear", value: "AC"},
+    {id: 'clear', value: 'AC'},
     {id: "del", value: "DEL"},
     {id: "divide", value: "/"},
     {id: "seven", value: "7"},
@@ -23,139 +22,107 @@ const buttons = [
     {id: "equals", value: "="},
 ];
 
-// The expression 3 + 5 * 6 - 2 / 4 should produce 32.5 or 11.5 as an
-//           answer, depending on the logic your calculator uses
-
 export default function App() {
-    const [output, setOutput] = useState('0');
+    const [output, setOutput] = useState('');
     const [entry, setEntry] = useState('');
-    const [lastOperator, setLastOperator] = useState(null);
-    const [lastValue, setLastValue] = useState(null);
+    const [lastValue, setLastValue] = useState('');
 
     function handleClick(value) {
         setLastValue(value)
+
+        if (value === '0' && lastValue === '0') return;
+        if (value === '.' && lastValue === '.') return;
+        if (isOperator(value) && isOperator(lastValue) && lastValue === value) return;
+
+        if (lastValue === '=' && isOperator(value)) {
+            setOutput(value);
+            setEntry(output + value);
+            return;
+        }
+
+        if (lastValue === '=' && isNumber(value)) {
+            setOutput(value);
+            setEntry(value);
+            return;
+        }
+
+        if (value === '=') {
+            calculate();
+            return;
+        }
 
         if (value === 'AC') {
             clearAll();
             return;
         }
 
-        if (value === 'CE') {
-            clearEntry();
+        if (value === 'DEL') {
+            clearLastEntry();
             return;
         }
 
-        if (value === 'del') {
-            deleteLast();
-            return;
-        }
+        if (isOperator(value) && isOperator(lastValue) && lastValue !== value) {
+            if (value === '-') {
+                setOutput(value);
+                setEntry(prevEntry => prevEntry + value);
+                return;
+            }
 
-        if (value === '=') {
-            calculate(value);
-            return;
-        }
-
-        if (lastValue === '=') {
-            setEntry('');
-            setOutput(value);
-            return;
-        }
-
-        if (isOperator(lastValue) && isOperator(value)) {
-            setLastOperator(value);
-            setEntry(prevEntry => prevEntry.slice(0, -1) + value);
-            return;
-        }
-
-        if (isNumber(value)) {
-            if (isOperator(lastValue)) {
-                if (value === '.') {
-                    setOutput('0.');
-                    return;
-                }
-
+            if (lastValue === '-') {
+                setEntry(prevEntry => prevEntry.slice(0, -2) + value)
                 setOutput(value);
                 return;
             }
 
-            if (value === '.' && output.includes('.')) {
-                return;
+            setOutput(value);
+            setEntry(entry.slice(0, -1) + value);
+            return;
+        }
+
+        if (isNumber(value)) {
+            if (value === '.' && (lastValue === '.' || lastValue === '')) {
+                value = '0.';
             }
 
-            if (value === '.') {
-                setOutput(prevOutput => prevOutput + value);
-                return;
+            if (entry.includes('.')) {
+                const lastNumber = entry.split(/[-+*/]/).pop();
+                if (lastNumber.includes('.') && value === '.') return;
             }
 
-            setOutput(prevOutput => prevOutput === '0' ? value : prevOutput + value);
+            setOutput(prevOutput => isOperator(prevOutput) ? value : prevOutput + value);
+            setEntry(prevEntry => prevEntry + value);
             return;
         }
 
         if (isOperator(value)) {
-            setLastOperator(value)
-
-            if (includesOperator(entry)) {
-                calculate(value);
-                return;
-            }
-            console.log("output", output, value)
-            console.log("output", output.includes('0.') && isOperator(value))
-            if (output.includes('0.') && isOperator(value)) {
-                setOutput('0');
-                setEntry(prevEntry => prevEntry + '0' + value);
-                return;
-            }
-
-            setEntry(prevEntry => prevEntry + output + value);
-            return;
+            setOutput(value);
+            setEntry(prevEntry => prevEntry + value);
         }
     }
 
     function clearAll() {
-        setOutput('0');
+        setOutput('');
         setEntry('');
-        setLastOperator(null);
-        setLastValue(null);
     }
-
-    function clearEntry() {
-        setOutput('0');
-    }
-
-    function deleteLast() {
-        setOutput(prevOutput => {
-            return prevOutput.length === 1 ? '0' : prevOutput.slice(0, -1);
-        });
-    }
-
-    function isOperator(value) {
-        return ['+', '-', '*', '/'].includes(value);
-    }
-
-    function includesOperator(value) {
-        const operators = ['+', '-', '*', '/'];
-
-        return operators.some(operator => value.includes(operator));
+    function clearLastEntry() {
+        setOutput(prevOutput => prevOutput.slice(0, -1));
+        setEntry(prevEntry => prevEntry.slice(0, -1));
     }
 
     function isNumber(value) {
         return !isNaN(value) || value === '.';
     }
 
-    function calculate(operator) {
+    function isOperator(value) {
+        return ['+', '-', '*', '/'].includes(value);
+    }
+
+    function calculate() {
         try {
-            const result = math.evaluate(entry + output);
+            const result = math.evaluate(entry);
             setOutput(result);
+            setEntry(prevEntry => prevEntry + '=' + result)
 
-
-            if (operator === '=') {
-                setEntry(prevEntry => {
-                    console.log("prevEntry", prevEntry)
-                    return prevEntry + output + operator;
-                });
-            } else {
-                setEntry(`${result}${operator}`);
-            }
 
         } catch (error) {
             console.error("Error occurred during calculation:", error);
@@ -175,7 +142,7 @@ export default function App() {
             <div className="calculator">
                 <div className="calculator__view">
                     <div className="calculator__formula">{entry}</div>
-                    <div id="display" className="calculator__output">{output}</div>
+                    <div id="display" className="calculator__output">{output ? output : 0}</div>
                 </div>
                 {renderButtons()}
             </div>
